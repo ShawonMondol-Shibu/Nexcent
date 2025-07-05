9f0223c1b86f3e7498599a864f4c11361f5af0a8
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-05 09:03:41 +0600

    design site updated

--- components/layout/DesignSite.tsx
+++ components/layout/DesignSite.tsx
@@ -15,7 +15,7 @@ export default function DesignSite() {
         />
         <div className="space-y-5 w-full m-auto">
           <h2 className="text-3xl font-bold">
-            How to design your site footer like we did
+            How to design your site footer like we did 
           </h2>
           <p>
            Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.
